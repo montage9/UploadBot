43c91624d8b0606e980a74cb1fdebbedbef26430
montage9 <105692684+montage9@users.noreply.github.com>
2023-09-16 01:55:20 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Pyrogram Client Arguments
-API_ID = ''
+API_ID = '@xray_me_bot'
 API_HASH = ''
-BOT_TOKEN = ''
+BOT_TOKEN = '6384751737:AAE4L-vQDfEFDCRTJy51LWc9GAyXoVBKEfQ'
 
 # Bot's Functionality Parameters
 MAX_FILE_SIZE = 2e8
